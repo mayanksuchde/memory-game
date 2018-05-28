a9ba03fcf1ae57d9861085439131e61e89c3e7ef
Mayank Suchde <mayanksuchde@Mayanks-MacBook-Pro.local>
2018-05-28 16:34:04 -0400

    add divs and shuffle bg style onClick

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import MemoryGame from './MemoryGame';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(
+	<MemoryGame />, 
+	document.getElementById('root'));
+
